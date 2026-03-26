e4f7f7adf1511420287661bc27b38f01616c2a92
Sukalpa Devkota <143915953+sukalpa0@users.noreply.github.com>
2026-03-26 08:35:27 +0545

    Add files via upload

---
+++ sumarr.js
@@ -0,0 +1,13 @@
+//Create an array of 5 numbers. Print the sum of all numbers using a loop.
+
+let num = [10, 20, 30, 40, 50];
+let sum = 0
+for(let i=0; i<num.length; i++){
+    
+     sum = sum + num[i];
+    
+}
+ console.log(sum);
+
+
+
---
+++ week3.js
@@ -0,0 +1,10 @@
+function login(user_name, password){
+    if(user_name=="Sukalpa" && password=="sukx123"){
+        console.log("Welcome Sukalpa");
+    }
+    else{
+        console.log("ko ho? chinena")
+    }
+}
+login("ha","ha");
+
